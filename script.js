5db62f2ccdb31740344ca4a30793014ada0ab538
ernalin26 <ernalin26@gmail.com>
2023-07-26 22:08:30 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//////////////////////////
+// Default Parameter
+
 const bookings = [];
 
 const createBooking = function (
@@ -26,3 +29,41 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 1000);
+
+
+/////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+// is the same as doing...
+const flightNum = flight;
+const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000)
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+
